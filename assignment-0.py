d097282ebe26194dda99827ce19949d56d1c5b0b
Hopeguy <jocke_ginste@live.com>
2021-06-14 19:15:53 +0200

    assignment-0

---
+++ assignment-0.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 14 18:01:15 2021
+
+@author: Joakim
+"""
+
+# Task 2
+
+Learning_list = ['basic components in a Python programme',
+'calculations using python',
+'use relevant models for advanced problems in physics and engineering',
+'solve problems using numerical algorithms',
+'represent data with graphics in python']
+
+a = 'I will learn something about '
+
+for i in Learning_list:
+    print(a + i)
+
